a6a406afc16d027feaa20adbaeccbe63e11a5bab
MonishwaranB <monishwaran@doodle-blue.com>
2026-02-07 17:28:59 +0530

    Fix db.json path and remove duplicate db

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const jsonServer = require("json-server");
+const path = require("path");
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(
+  path.join(__dirname, "db.json")
+);
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 10000;
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-
-server.use("/users", router);
+server.use(router);
 
 server.listen(PORT, () => {
-  console.log("JSON Server running on port " + PORT);
+  console.log("JSON Server running on port", PORT);
 });
